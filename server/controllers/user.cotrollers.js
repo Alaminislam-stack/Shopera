5e603e6fe2759eae2c372ad8f6e5fe8c9dfe6131
Alamin <alamin890213@gmail.com>
2025-12-02 00:22:34 +0600

    feat: Implement user authentication and profile management with register, login, Google OAuth, profile, and logout functionalities.

--- server/controllers/user.cotrollers.js
+++ server/controllers/user.cotrollers.js
@@ -69,8 +69,8 @@ export const register = asyncHandler(async (req, res, next) => {
   // Set cookie
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   });
@@ -122,9 +122,8 @@ export const registerWithGoogle = asyncHandler(async (req, res, next) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "none",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       path: "/",
     });
 
@@ -220,8 +219,8 @@ export const login = asyncHandler(async (req, res, next) => {
   // Set cookie
   res.cookie("token", token, {
     httpOnly: true,
-    secure: true,
-    sameSite: "none",
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: "/",
   });
